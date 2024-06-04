662e200ec45cc793e85a5790645c0096c9757b29
Cedric <cedricwahrmund@gmail.com>
2024-06-04 17:37:19 -0500

    Remove useless function, change args to have simpler names

--- renamer.py
+++ renamer.py
@@ -6,21 +6,18 @@ from subprocess import call
 
 # Parse args using argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("target_str", help="string to be replaced, should be one word", type=str)
-parser.add_argument("new_str", help="new string to replace the target_str with", type=str)
+parser.add_argument("old", help="string to be replaced, should be one word", type=str)
+parser.add_argument("new", help="new string to replace the target_str with", type=str)
 parser.add_argument("directory", nargs="?", help="directory to replace strings in, defaults to CWD", type=str, default=os.getcwd())
 parser.add_argument("-v", "--verbose", help="enable more verbose output", action="store_true")
 args = parser.parse_args()
 
 verboseprint = print if args.verbose else lambda *a, **k: None
 
-def print_usage():
-    pass
-
 def main():
     verboseprint("This is verbose")
-    print("Original name:", args.target_str)
-    print("New name:", args.new_str)
+    print("Original name:", args.old)
+    print("New name:", args.new)
     print("Directory:", args.directory)
 
 
