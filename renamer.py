1f70452f4479da47619e484e9fc7b8fa1e4e93eb
Cedric <cedricwahrmund@gmail.com>
2024-06-04 17:33:49 -0500

    Added argparse to parse the needed arguments, will add more
    Currently, the argparse handles the positonal, required arguments
    target_str and new_str and the positional, optional argument of
    the directory to replace in (defaults to cwd). It also can handle
    the optional verbose flag.

--- renamer.py
+++ renamer.py
@@ -1,9 +1,27 @@
 import os
+import sys
+import argparse
 import tarfile
-from subprocesses import call
+from subprocess import call
+
+# Parse args using argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("target_str", help="string to be replaced, should be one word", type=str)
+parser.add_argument("new_str", help="new string to replace the target_str with", type=str)
+parser.add_argument("directory", nargs="?", help="directory to replace strings in, defaults to CWD", type=str, default=os.getcwd())
+parser.add_argument("-v", "--verbose", help="enable more verbose output", action="store_true")
+args = parser.parse_args()
+
+verboseprint = print if args.verbose else lambda *a, **k: None
+
+def print_usage():
+    pass
 
 def main():
-    pass
+    verboseprint("This is verbose")
+    print("Original name:", args.target_str)
+    print("New name:", args.new_str)
+    print("Directory:", args.directory)
 
 
 if __name__ == "__main__":
